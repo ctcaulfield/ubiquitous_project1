1f9c042632f5a416a4e551dea17a6e69103a71ef
ctcaulfield <ctcaulfield@gmail.com>
2017-02-19 02:19:19 -0500

    Comparison is working

--- gameClient/main.js
+++ gameClient/main.js
@@ -25,65 +25,33 @@ ws.addEventListener("message", function(e) {
  	 	setPlayer(message[0]);
  	}
  	addWeapon(message);
-
- // 	console.log("0:" + message[0]);
- // 	console.log("1:"+ message[1]);
- // 	//add element to hashmap
-
- // 	//if(weaponChoices.length >= 1){
- // 	// if(weaponChoices.hashOwnProperty(message[0])) {
-
- // 	// } else {
- // 		weaponChoices[message[0]] = message[1];
- // 	// }
- // 	console.log("Size of Hash: " + weaponChoices.length);
- // 	$.each(weaponChoices, function(index,value){
-	// 	console.log("Index = " + index + " value = " + value); 
-	// })
-
- // 	if(weaponChoices.length == 1){
- // 		console.log("weaponChoice's called");
- // 		compareResults(weaponChoices);
- // 	}		
+	
 });
 
 
 
 //create the player - this will only be done once for each bean connected
 function setPlayer(data){
-
 	//player div with id of their username from bean
 	var playerDiv = document.createElement("div");
 	playerDiv.id = data;
-	playerDiv.innerHTML = "<h2>"+data+"</h2>";
+	playerDiv.innerHTML = "<h3>"+data+"</h3>";
 
 	//add playerDiv to parent div of all players
 	var players = document.getElementById("players");
 	players.appendChild(playerDiv);
-	
+
 	var weaponDiv = document.createElement("div");
-	weaponDiv.id = data+"_weapon";
-	playerDiv.appendChild(weaponDiv);
+	weaponDiv.className = "weapon";
 
-
-	var scoreDiv = document.createElement("div");
-	scoreDiv.id = data+"_score";
-	playerDiv.appendChild(scoreDiv);
+	document.getElementById(data).appendChild(weaponDiv);
 }
 
 //gets the players duel selection and sets it to their "DrawHand"
 function addWeapon(message){
-	var player = document.getElementById(message[0]+"_weapon");
-	player.innerHTML = "<h2>"+message[1]+"</h2>";
+	var player = document.getElementById(message[0]);
+	player.getElementsByClassName("weapon")[0].innerHTML = "<h2>"+message[1]+"</h2>";
 }
-
-//logic will go here of determing who won the round
-function letsDuel(){
-	console.log("determing who won");
-	compareResults();
-	// ws.send("DUEL");
-}
-
 
 
 //clicker = 2
@@ -92,15 +60,21 @@ function letsDuel(){
 //2 beats 0, 0 beats 1, 1 beats 2
 
 function compareResults(){
-	//"MaryRoseBean:1"
-	//"CCSONIC:2"
-        // Compare user choice vs computer choice
 
-    var choice1 = document.getElementById("players").children[0];
+    var players = document.getElementById("players");
+    
+    //player 1 info
+    var weapon1 = document.getElementsByClassName("weapon")[0];
+    var text1 = weapon1.getElementsByTagName("h2")[0].innerHTML;
+    console.log("1: "+text1);
 
-    var choice2 = document.getElementById("players").children[1];
-    console.log("WORK!?!?!?!??!?!?!")
-    console.log("Compare results: " + choice1 + " " + choice2);
+    //player 2 info
+    var weapon2 = document.getElementsByClassName("weapon")[1];
+    var text2 = weapon2.getElementsByTagName("h2")[0].innerHTML;
+    console.log("2: "+text2);
+
+    console.log("WORK!?!?!?!??!?!?!");
+    console.log("Compare results: " + text1 + " and " + text2);
 
 	// if(choice1 === "4" && choice2 === "4") {
 	// 	return "Nobody Wins"
@@ -149,12 +123,12 @@ function compareResults(){
 $("#countdown").countdown360(
 	{
 		radius      : 40,
-	  	seconds     : 20,
+	  	seconds     : 5,
 	  	strokeWidth : 5,
 	  	fillStyle   : '#0276FD',
 	  	strokeStyle : '#003F87',
 	  	fontSize    : 30,
 	  	fontColor   : '#FFFFFF',
-	  	autostart: true,
-	  	//onComplete  : function () { letsDuel(); }
+	  	autostart: false,
+	  	onComplete  : function () { compareResults(); }
 }).start()
